4824e49bc44cf034ced3ebe9413b14b5ea309fce
ashwith26 <ashwith26@gmail.com>
2025-02-17 15:04:21 +0530

    added wishlist for similar products, back button and fixed virtualized list error

--- Screens/ProductDetails.js
+++ Screens/ProductDetails.js
@@ -4,7 +4,6 @@ import {
   Text,
   Image,
   StyleSheet,
-  ScrollView,
   TouchableOpacity,
   FlatList,
   Alert,
@@ -14,28 +13,26 @@ import {
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import HeartIcon from "./HeartIcon";
 import { useNavigation } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useWishlist } from "./WishlistContext"; // Import the useWishlist hook
+import { useWishlist } from "./WishlistContext";
 
 const ProductDetailsScreen = ({ route }) => {
   const { product, allProducts } = route.params;
   const navigation = useNavigation();
-  const { wishlist, toggleWishlist } = useWishlist(); // Use the wishlist context
+  const { wishlist, toggleWishlist } = useWishlist();
   const [isInWishlist, setIsInWishlist] = useState(false);
 
-  // Check if the product is in the wishlist
   useEffect(() => {
     const isProductInWishlist = wishlist.some((item) => item.id === product.id);
     setIsInWishlist(isProductInWishlist);
   }, [wishlist, product.id]);
 
-  // Function to handle adding/removing a product from the wishlist
   const handleWishlistToggle = () => {
-    toggleWishlist(product); // Add or remove the product from the wishlist
+    toggleWishlist(product);
   };
 
-  // Function to handle adding a product to the cart
   const handleAddToCart = async () => {
     try {
       const cartData = await AsyncStorage.getItem("cart");
@@ -59,156 +56,162 @@ const ProductDetailsScreen = ({ route }) => {
     }
   };
 
-  // Filter similar products based on the category of the current product
   const similarProducts = allProducts.filter(
     (item) => item.category === product.category && item.id !== product.id
   );
 
-  return (
-    <>
-    <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        {/* Product Image */}
-        <View style={styles.productContainer}>
-          <Image source={product.image} style={styles.image} />
-        </View>
-        <View>
-          <Text style={styles.category}>{product.category}</Text>
-          <Text style={styles.name}>{product.name}</Text>
-          <Text style={styles.price}>{product.price}</Text>
-        </View>
+  const modifiedIconStyle = {
+    position: "absolute",
+    top: 9,
+    right: -7, 
+    zIndex: 15, 
+  };
 
-        {/* Product Details */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Product Details</Text>
-          <View style={styles.detailsRow}>
-            <View style={styles.detailsColumn}>
-              <Text style={styles.detailLabel}>Product Name:</Text>
-              <Text style={styles.detailValue}>{product.name}</Text>
-            </View>
-            <View style={styles.detailsColumn}>
-              <Text style={styles.detailLabel}>Material:</Text>
-              <Text style={styles.detailValue}>{product.material}</Text>
-            </View>
-          </View>
-          <View style={styles.detailsRow}>
-            <View style={styles.detailsColumn}>
-              <Text style={styles.detailLabel}>Care:</Text>
-              <Text style={styles.detailValue}>{product.Care}</Text>
-            </View>
-            <View style={styles.detailsColumn}>
-              <Text style={styles.detailLabel}>Colour:</Text>
-              <Text style={styles.detailValue}>{product.colour}</Text>
-            </View>
-          </View>
-        </View>
+  const renderSimilarItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.item}
+      onPress={() => {
+        navigation.push("ProductDetails", {
+          product: item,
+          allProducts: allProducts,
+        });
+      }}
+    >
+      <HeartIcon item={item} iconStyle={modifiedIconStyle} />
+      <Image source={item.image} style={styles.image1} />
+      <Text style={styles.name1}>{item.name}</Text>
+      <Text style={styles.price1}>{item.price}</Text>
+    </TouchableOpacity>
+  );
 
-        {/* Delivery Address */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Delivery Address</Text>
-          <View style={styles.deliveryContainer}>
-            <View style={styles.addressContainer}>
-              <Text style={styles.deliveryAddress}>
-                Delivery to:{" "}
-                <Text style={styles.addressName}>Shambavi, 518003</Text>
-              </Text>
-              <Text style={styles.addressText}>
-                Kallur Estate Near Shukalamma Temple, Nagula Chatu, Kallur,
-                Kurnool
-              </Text>
-            </View>
-            <TouchableOpacity style={styles.changeButton}>
-              <Text style={styles.changeButtonText}>CHANGE</Text>
-            </TouchableOpacity>
+  const listHeader = (
+    <View>
+      <TouchableOpacity
+        style={styles.backbutton}
+        onPress={() => navigation.goBack()}
+      >
+        <Image
+          source={require("../assets/productdetails/backbutton.png")}
+        />
+      </TouchableOpacity>
+      <View style={styles.productContainer}>
+        <Image source={product.image} style={styles.image} />
+      </View>
+      <View>
+        <Text style={styles.category}>{product.category}</Text>
+        <Text style={styles.name}>{product.name}</Text>
+        <Text style={styles.price}>{product.price}</Text>
+      </View>
+
+      {/* Product Details */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Product Details</Text>
+        <View style={styles.detailsRow}>
+          <View style={styles.detailsColumn}>
+            <Text style={styles.detailLabel}>Product Name:</Text>
+            <Text style={styles.detailValue}>{product.name}</Text>
+          </View>
+          <View style={styles.detailsColumn}>
+            <Text style={styles.detailLabel}>Material:</Text>
+            <Text style={styles.detailValue}>{product.material}</Text>
           </View>
         </View>
+        <View style={styles.detailsRow}>
+          <View style={styles.detailsColumn}>
+            <Text style={styles.detailLabel}>Care:</Text>
+            <Text style={styles.detailValue}>{product.care}</Text>
+          </View>
+          <View style={styles.detailsColumn}>
+            <Text style={styles.detailLabel}>Colour:</Text>
+            <Text style={styles.detailValue}>{product.colour}</Text>
+          </View>
+        </View>
+      </View>
 
-        {/* Delivery & Return Details */}
-        <View style={styles.section}>
-          <Text style={styles.sectionTitle}>Delivery and Return Details</Text>
-          <View style={styles.deliveryDetailRow}>
-            <Image
-              source={require("../assets/productdetails/deliverIcon.png")}
-              style={styles.deliveryIcon}
-            />
-            <Text style={styles.deliveryText}>
-              Delivery within <Text style={styles.boldText}>3 hours</Text>.
-              Express shipping options available for urgent deliveries.
+      {/* Delivery Address */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Delivery Address</Text>
+        <View style={styles.deliveryContainer}>
+          <View style={styles.addressContainer}>
+            <Text style={styles.deliveryAddress}>
+              Delivery to:{" "}
+              <Text style={styles.addressName}>Shambavi, 518003</Text>
+            </Text>
+            <Text style={styles.addressText}>
+              Kallur Estate Near Shukalamma Temple, Nagula Chatu, Kallur,
+              Kurnool
             </Text>
           </View>
-          <View style={styles.deliveryDetailRow}>
-            <Image
-              source={require("../assets/productdetails/returnicon.png")}
-              style={styles.deliveryIcon}
-            />
-            <Text style={styles.deliveryText}>
-              <Text style={styles.boldText}>Non-Returnable Product</Text>. Once
-              purchased, it cannot be returned or exchanged.
-            </Text>
-          </View>
+          <TouchableOpacity style={styles.changeButton}>
+            <Text style={styles.changeButtonText}>CHANGE</Text>
+          </TouchableOpacity>
         </View>
+      </View>
 
-        {/* Similar Products */}
-        <View style={styles.similarsection}>
-          <Text style={styles.similarsectionTitle}>Similar Products</Text>
-          <FlatList
-            data={similarProducts}
-            keyExtractor={(item) => item.id}
-            numColumns={2}
-            renderItem={({ item }) => (
-              <TouchableOpacity
-                style={styles.item}
-                onPress={() => {
-                  navigation.push("ProductDetails", { // Use navigation.push
-                    product: item,
-                    allProducts: allProducts,
-                  });
-                }}
-              >
-                <Image source={item.image} style={styles.image1} />
-                <Text style={styles.name1}>{item.name}</Text>
-                <Text style={styles.price1}>{item.price}</Text>
-              </TouchableOpacity>
-            )}
-          />
-        </View>
-      </ScrollView>
-
-      {/* Fixed Bottom Buttons */}
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={[styles.heartButton]}
-          onPress={handleWishlistToggle} // Toggle wishlist
-        >
-          <Icon
-            name="heart"
-            color={isInWishlist ? "red" : "#bfbfbf"}
-            style={[styles.heartIcon]}
-          />
-        </TouchableOpacity>
-        {/* <TouchableOpacity
-          style={[
-            styles.heartButton,
-            { color: isInWishlist ? "red" : "black" },
-          ]}
-          onPress={handleWishlistToggle} // Toggle wishlist
-        >
+      {/* Delivery & Return Details */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Delivery and Return Details</Text>
+        <View style={styles.deliveryDetailRow}>
           <Image
-            source={require("../assets/productdetails/heartIcon.png")}
-            style={[
-              styles.heartButton,
-              { color: isInWishlist ? "red" : "black" },
-            ]}
-          ></Image>
-        </TouchableOpacity> */}
-        <TouchableOpacity
-          style={styles.addToCartButton}
-          onPress={handleAddToCart}
-        >
-          <Text style={styles.addToCartText}>ADD TO CART</Text>
-        </TouchableOpacity>
+            source={require("../assets/productdetails/deliverIcon.png")}
+            style={styles.deliveryIcon}
+          />
+          <Text style={styles.deliveryText}>
+            Delivery within <Text style={styles.boldText}>3 hours</Text>.
+            Express shipping options available for urgent deliveries.
+          </Text>
+        </View>
+        <View style={styles.deliveryDetailRow}>
+          <Image
+            source={require("../assets/productdetails/returnicon.png")}
+            style={styles.deliveryIcon}
+          />
+          <Text style={styles.deliveryText}>
+            <Text style={styles.boldText}>Non-Returnable Product</Text>. Once
+            purchased, it cannot be returned or exchanged.
+          </Text>
+        </View>
+      </View>
+
+      {/* Similar Products Title */}
+      <View style={styles.similarsection}>
+        <Text style={styles.similarsectionTitle}>Similar Products</Text>
       </View>
     </View>
+  );
+
+  return (
+    <>
+      <View style={styles.container}>
+        <FlatList
+          data={similarProducts}
+          renderItem={renderSimilarItem}
+          keyExtractor={(item) => item.id}
+          numColumns={2}
+          ListHeaderComponent={listHeader}
+          contentContainerStyle={styles.listContainer}
+        />
+
+        {/* Fixed Bottom Buttons */}
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.heartButton]}
+            onPress={handleWishlistToggle}
+          >
+            <Icon
+              name="heart"
+              color={isInWishlist ? "red" : "#bfbfbf"}
+              style={[styles.heartIcon]}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.addToCartButton}
+            onPress={handleAddToCart}
+          >
+            <Text style={styles.addToCartText}>ADD TO CART</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 };
@@ -218,15 +221,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
-  scrollContainer: {
-    paddingBottom: hp("10%"),
-    paddingHorizontal: wp(5),
-  },
+
   productContainer: {
     alignItems: "center",
   },
+  backbutton: {
+    position: "absolute",
+    zIndex: 1,
+    top: 20,
+    left: 20,
+    width: wp(5),
+    height: wp(5),
+    objectFit: "contain",
+  },
   image: {
-    marginTop: hp(8),
     width: wp(90),
     height: hp(50),
     borderTopLeftRadius: wp(5),
@@ -236,12 +244,12 @@ const styles = StyleSheet.create({
     fontSize: wp(4.4),
     fontWeight: "bold",
     marginTop: hp(2),
-    paddingLeft: hp(0.2),
+    paddingHorizontal: hp(1),
   },
   name: {
     fontSize: wp(4.2),
     marginTop: hp(0.2),
-    paddingLeft: hp(0.3),
+    paddingHorizontal: hp(0.5),
   },
   price: {
     fontSize: wp(4.3),
@@ -249,12 +257,12 @@ const styles = StyleSheet.create({
     color: "black",
     marginTop: hp(0.2),
     marginBottom: hp(),
-    paddingLeft: hp(0.3),
+    paddingHorizontal: hp(0.5),
   },
   section: {
     marginTop: hp(3),
     marginBottom: hp(1),
-    paddingLeft: hp(0.2),
+    paddingHorizontal: hp(0.5),
   },
   sectionTitle: {
     fontSize: wp(4.5),
@@ -351,6 +359,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 1,
     borderColor: "#E0E0E0",
     backgroundColor: "white",
+    boxShadow: "0px -2px 4px rgba(0, 0, 0, 0.1)",
   },
   heartButton: {
     width: wp(12),
@@ -362,7 +371,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   heartIcon: {
-    fontSize: wp(6.5)
+    fontSize: wp(6.5),
   },
   addToCartButton: {
     backgroundColor: "#47154B",
@@ -380,19 +389,23 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginTop: hp(2),
     marginBottom: hp(3),
+    marginHorizontal: hp(0.5),
   },
-  // listContainer: {
-  //   paddingTop: hp(3),
-  // },
+  listContainer: {
+    paddingHorizontal: hp(2),
+    paddingTop: hp(7),
+    paddingBottom: hp(10),
+  },
   item: {
-    width: wp(45),
+    width: wp(35),
     marginBottom: hp(3),
     alignItems: "center",
+    marginLeft: wp(4.5),
+    marginRight: wp(8),
   },
   image1: {
-    width: wp(40),
-    height: wp(55),
-    // borderRadius: wp(2),
+    width: wp(43),
+    height: wp(57),
   },
   name1: {
     fontSize: wp(4),
@@ -406,8 +419,11 @@ const styles = StyleSheet.create({
     color: "black",
     marginTop: hp(0.5),
     textAlign: "center",
-    // marginBottom: hp(1.5),
   },
+  // heartContainer: { top: 14, right: 12 },
+  // // heartIcon1: {
+  // //   marginLeft: wp(),
+  // // }
 });
 
 export default ProductDetailsScreen;
